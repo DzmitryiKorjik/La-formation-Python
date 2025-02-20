6ac22c702c73532c937a122546f999fea567ccfc
DzmitryiKorjik <dmardovitch@gmail.com>
2025-02-20 11:07:27 +0100

    test html

--- TestUnitaire/test/test_main.py
+++ TestUnitaire/test/test_main.py
@@ -6,7 +6,6 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from main import add
 
-
 def test_add_with_two_numbers():
     assert add(5, 10) == 15
 
